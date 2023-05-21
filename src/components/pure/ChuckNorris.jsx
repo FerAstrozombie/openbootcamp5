f6ae1a81a783d28663761632fd816e441bcadbd1
flair <fernando_punk77@hotmail.com>
2023-05-21 08:21:01 -0300

    like/unlike

--- src/components/pure/ChuckNorris.jsx
+++ src/components/pure/ChuckNorris.jsx
@@ -1,14 +1,13 @@
-import { useState, useEffect } from "react";
-import getRandomJokes from "../../services/chuckNorris"
+import { useState } from "react";
+import getRandomJokes from "../../services/chuckNorris";
+import Button from '@mui/material/Button';
 
 
 const ChuckNorris = () => {
 
     const [jokes, setJokes] = useState("");
-
-    /* useEffect(() => {
-        obtainJokes()
-    }, []); */
+    const [like, setLike] = useState(0);
+    const [unlike, setUnlike] = useState(0);
 
     const obtainJokes = () => {
         getRandomJokes()
@@ -20,19 +19,40 @@ const ChuckNorris = () => {
             .catch((error) => alert("Hubo un error:", error))
     }
 
+    const nextLikeJoke = () => {
+        obtainJokes();
+        setLike(like + 1);
+    }
+    const nextUnlikeJoke = () => {
+        obtainJokes();
+        setUnlike(unlike + 1);
+    }
+    
     return (
         <div>
             <h2>Chuck Norris jokes</h2>
             {jokes ?
                 (
                     <div>
-                        <h4>{jokes}</h4>
-                        <button onClick={obtainJokes}>Generate joke</button>
+                        <div className="jokes">
+                            <h4>{jokes}</h4>
+                        </div>
+                        <div className="chistes">
+                            <Button variant="contained" onClick={nextLikeJoke}>Like Joke</Button>
+                            <Button variant="contained" onClick={nextUnlikeJoke}>Unlike joke</Button>
+                        </div>
+                        <div className="like">
+                            <h4>Chistes que te han gustado: {like}</h4>
+                            <h4>Chistes que no te han gustado: {unlike}</h4>
+                        </div>
+
                     </div>
                 )
                 :
                 (
-                    <button onClick={obtainJokes}>Generate joke</button>
+                    <div className="jokes">
+                        <Button variant="contained" onClick={obtainJokes}>Generate joke</Button>
+                    </div>
                 )
             }
         </div>
